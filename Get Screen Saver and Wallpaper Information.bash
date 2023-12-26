d899f675e7aa5846f5c6cfb6e5bb53242eac4ca8
itjimbo <35304640+itjimbo@users.noreply.github.com>
2023-12-26 14:53:03 -0500

    Create Get Screen Saver and Wallpaper Information.bash

---
+++ Get Screen Saver and Wallpaper Information.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+loggedInUser=$(/usr/bin/stat -f%Su /dev/console)
+
+wallpaper_store_path="/Users/${loggedInUser}/Library/Application Support/com.apple.wallpaper/Store/Index.plist"
+
+getWallpaperBase64=$(plutil -extract AllSpacesAndDisplays xml1 -o - "${wallpaper_store_path}" | awk '/<data>/,/<\/data>/' | xargs | tr -d " " | tr "<" "\n" | head -2 | tail -1 | cut -c6-)
+echo "wallpaperBase64: ${getWallpaperBase64}"
+
+getWallpaperLocation=$(plutil -extract AllSpacesAndDisplays xml1 -o - "${wallpaper_store_path}" | grep -A 2 "relative" | head -2 | tail -1 | xargs | cut -c9- | rev | cut -c10- | rev)
+echo "wallpaperLocation: ${getWallpaperLocation}"
+
+getScreensaverBase64=$(plutil -extract AllSpacesAndDisplays xml1 -o - "${wallpaper_store_path}" | awk '/<data>/,/<\/data>/' | xargs | tr -d " " | tr "<" "\n" | tail -2 | head -1 | cut -c6-)
+echo "screenSaverBase64: ${getScreensaverBase64}"
